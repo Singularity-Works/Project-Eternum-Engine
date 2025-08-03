cc56a5e2c817accf79bde69fc32824e38828f2b1
Jax <jax.clayton@digipen.edu>
2025-08-02 17:01:03 -0700

    deleted build

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/nekoy/Desktop/Project-Eternum-Engine
-# Build directory: C:/Users/nekoy/Desktop/Project-Eternum-Engine/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(EternumTests "C:/Users/nekoy/Desktop/Project-Eternum-Engine/cmake-build-debug/eternum-tests.exe")
-set_tests_properties(EternumTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/nekoy/Desktop/Project-Eternum-Engine/CMakeLists.txt;21;add_test;C:/Users/nekoy/Desktop/Project-Eternum-Engine/CMakeLists.txt;0;")
