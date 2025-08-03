64d0f42c4d38cb25721f2dbee1445961b6afb376
SixSleepyAsh <Claytonjax88@gmail.com>
2025-08-02 20:29:52 -0700

    Add GoogleTest as a vendored dependency
    Vendored GoogleTest under Eternum Engine/third_party/googletest and updated CMakeLists.txt to build and link unit tests with gtest_main. Adjusted test source file globbing and include directories to support the new test structure. Updated .gitignore to exclude .idea directory.

---
+++ Unit Tests/SystemTests.cpp
@@ -0,0 +1,20 @@
+﻿#include <gtest/gtest.h>
+#include  <Systems/system.h>
+
+class TestSystem : public System {
+public:
+    TestSystem() : System("TestSystem") {}
+    bool initCalled = false;
+    void Init() override { initCalled = true; }
+};
+
+TEST(SystemTests, NameIsSetCorrectly) {
+    TestSystem sys;
+    EXPECT_EQ(sys.GetName(), "TestSystem");
+}
+
+TEST(SystemTests, InitIsCallable) {
+    TestSystem sys;
+    sys.Init();
+    EXPECT_TRUE(sys.initCalled);
+}
