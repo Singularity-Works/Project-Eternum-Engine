51aa63041a48b778e75e67f114552b234cb3ce7b
Jax <jax.clayton@digipen.edu>
2026-07-09 20:52:00 -0700

    Add Component implementation

---
+++ Eternum Engine/src/Core/ECS/Component/Component.cpp
@@ -0,0 +1,24 @@
+﻿/*******************************************************************************************
+* Project Eternum Engine
+* -----------------------------------------------------------------------------------------
+* File: Component
+* Description:
+*     Out of line parts of the base Component class, the ones that need a complete Entity.
+*
+* Author:     Jax Clayton
+* Created:    8/6/2025
+* License:    MIT License (see LICENSE file in project root)
+*******************************************************************************************/
+
+#include <pch.h>
+#include "Component.h"
+#include <Core/ECS/Entity/Entity.h>
+
+std::string Component::GetName() const
+{
+    // an unparented Component still deserves a readable name
+    if ( m_Parent == nullptr )
+        return PrefixlessName( m_Type );
+
+    return m_Parent->GetName() + "->" + PrefixlessName( m_Type );
+}
