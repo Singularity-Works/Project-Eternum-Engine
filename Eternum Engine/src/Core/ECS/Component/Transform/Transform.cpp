db5c041dc2d3818d6d80c277193028d4b00ead7d
Jax <jax.clayton@digipen.edu>
2026-07-12 15:03:00 -0700

    Move Transform onto Vector and widen its interface

--- Eternum Engine/src/Core/ECS/Component/Transform/Transform.cpp
+++ Eternum Engine/src/Core/ECS/Component/Transform/Transform.cpp
@@ -3,7 +3,7 @@
 * -----------------------------------------------------------------------------------------
 * File: Transform
 * Description:
-*     TODO: Describe the purpose of this file.
+*     Position, rotation and scale for an Entity.
 *
 * Author:     Jax Clayton
 * Created:    8/8/2025
@@ -12,3 +12,94 @@
 
 #include <pch.h>
 #include "Transform.h"
+#include <nlohmann/json.hpp>
+#include <Core/ECS/Component/ComponentFactory.h>
+
+//-----------------------------------------------------------------------------
+// Constructor / Destructor
+//-----------------------------------------------------------------------------
+
+Transform::Transform() = default;
+
+Transform::Transform( Vec2f const& translation, const float rotation, Vec2f const& scale ) :
+    m_Translation( translation ),
+    m_Rotation( rotation ),
+    m_Scale( scale )
+{}
+
+//-----------------------------------------------------------------------------
+// Public Methods
+//-----------------------------------------------------------------------------
+
+void Transform::Translate( Vec2f const& offset )
+{
+    m_Translation += offset;
+    m_IsDirty = true;
+}
+
+void Transform::Rotate( const float degrees )
+{
+    SetRotation( m_Rotation + degrees );
+}
+
+void Transform::Inspect()
+{
+    std::cout << GetName()
+              << " translation " << m_Translation
+              << " rotation " << m_Rotation
+              << " scale " << m_Scale
+              << std::endl;
+}
+
+//-----------------------------------------------------------------------------
+// Public Accessors
+//-----------------------------------------------------------------------------
+
+void Transform::SetTranslation( Vec2f const& translation )
+{
+    m_Translation = translation;
+    m_IsDirty = true;
+}
+
+void Transform::SetRotation( const float rotation )
+{
+    // keep rotation readable instead of letting it run away
+    m_Rotation = std::fmod( rotation, 360.0f );
+    if ( m_Rotation < 0.0f )
+        m_Rotation += 360.0f;
+
+    m_IsDirty = true;
+}
+
+void Transform::SetScale( Vec2f const& scale )
+{
+    m_Scale = scale;
+    m_IsDirty = true;
+}
+
+//-----------------------------------------------------------------------------
+// Serialization
+//-----------------------------------------------------------------------------
+
+void Transform::Write( nlohmann::json& data ) const
+{
+    data[ "translation" ] = { m_Translation.x(), m_Translation.y() };
+    data[ "rotation" ] = m_Rotation;
+    data[ "scale" ] = { m_Scale.x(), m_Scale.y() };
+}
+
+void Transform::Read( nlohmann::json const& data )
+{
+    if ( data.contains( "translation" ) && data[ "translation" ].size() >= 2 )
+        SetTranslation( Vec2f{ data[ "translation" ][ 0 ].get< float >(),
+                               data[ "translation" ][ 1 ].get< float >() } );
+
+    if ( data.contains( "rotation" ) )
+        SetRotation( data[ "rotation" ].get< float >() );
+
+    if ( data.contains( "scale" ) && data[ "scale" ].size() >= 2 )
+        SetScale( Vec2f{ data[ "scale" ][ 0 ].get< float >(),
+                         data[ "scale" ][ 1 ].get< float >() } );
+}
+
+REGISTER_COMPONENT(Transform)
--- Eternum Engine/src/Core/ECS/Component/Transform/Transform.h
+++ Eternum Engine/src/Core/ECS/Component/Transform/Transform.h
@@ -3,7 +3,8 @@
 * -----------------------------------------------------------------------------------------
 * File: Transform
 * Description:
-*     TODO: Describe the purpose of this file.
+*     Position, rotation and scale for an Entity. The first real Component in the engine
+*     and the reference for how a Component should be written.
 *
 * Author:     Jax Clayton
 * Created:    8/8/2025
@@ -16,7 +17,8 @@
 #include <Core/ECS/Component/Component.h>
 #include <Systems/ComponentSystem/ComponentSystem.h>
 
-class Transform final : public Component{
+class Transform final : public ComponentOf< Transform >
+{
 
 public:
     //-----------------------------------------------------------------------------
@@ -26,8 +28,73 @@ public:
     /// @brief  constructor
     Transform();
 
+    /// @brief  constructor
+    /// @param  translation the starting position
+    /// @param  rotation    the starting rotation in degrees
+    /// @param  scale       the starting scale
+    explicit Transform( Vec2f const& translation, float rotation = 0.0f, Vec2f const& scale = Vec2f( 1.0f ) );
+
     /// @brief destructor
     ~Transform() override = default;
+
+    //-----------------------------------------------------------------------------
+    // Public Methods
+    //-----------------------------------------------------------------------------
+
+    /// @brief  moves this Transform by an offset
+    /// @param  offset  how far to move
+    void Translate( Vec2f const& offset );
+
+    /// @brief  turns this Transform by an amount
+    /// @param  degrees how far to turn in degrees
+    void Rotate( float degrees );
+
+    /// @brief  Used by the Inspection System to display information about this Component
+    void Inspect() override;
+
+    /// @brief  writes this Component's state so it can be saved
+    /// @param  data    the object to write into
+    void Write( nlohmann::json& data ) const override;
+
+    /// @brief  reads this Component's state back out of a save
+    /// @param  data    the object to read from
+    void Read( nlohmann::json const& data ) override;
+
+
+    //-----------------------------------------------------------------------------
+    // Public Accessors
+    //-----------------------------------------------------------------------------
+
+    /// @brief  gets the position of this Transform
+    /// @return the position of this Transform
+    Vec2f const& GetTranslation() const { return m_Translation; }
+
+    /// @brief  sets the position of this Transform
+    /// @param  translation the new position
+    void SetTranslation( Vec2f const& translation );
+
+    /// @brief  gets the rotation of this Transform in degrees
+    /// @return the rotation of this Transform in degrees
+    float GetRotation() const { return m_Rotation; }
+
+    /// @brief  sets the rotation of this Transform
+    /// @param  rotation    the new rotation in degrees
+    void SetRotation( float rotation );
+
+    /// @brief  gets the scale of this Transform
+    /// @return the scale of this Transform
+    Vec2f const& GetScale() const { return m_Scale; }
+
+    /// @brief  sets the scale of this Transform
+    /// @param  scale   the new scale
+    void SetScale( Vec2f const& scale );
+
+    /// @brief  gets whether this Transform changed since the last time it was cleared
+    /// @return whether this Transform needs its matrix regenerated
+    bool IsDirty() const { return m_IsDirty; }
+
+    /// @brief  marks this Transform as up to date
+    void ClearDirty() { m_IsDirty = false; }
 
 private:
     //-----------------------------------------------------------------------------
@@ -41,12 +108,11 @@ private:
     float m_Rotation = 0.0f;
 
     /// @brief the scale of this Transform
-    Vec2f m_Scale = Vec2f::Zero();
+    Vec2f m_Scale = Vec2f( 1.0f );
 
     /// @brief  flag for when the matrix needs to be regenerated
     bool m_IsDirty = true;
 
 };
 
-REGISTER_COMPONENT_SYSTEM(Transform)
 #endif //TRANSFORM_H
