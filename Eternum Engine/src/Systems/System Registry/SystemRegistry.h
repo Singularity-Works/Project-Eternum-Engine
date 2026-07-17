347d8410c1e94790d29066032a73de3675729578
Jax <jax.clayton@digipen.edu>
2026-07-16 20:02:00 -0700

    Drop the component system registration macro

--- Eternum Engine/src/Systems/System Registry/SystemRegistry.h
+++ Eternum Engine/src/Systems/System Registry/SystemRegistry.h
@@ -64,17 +64,6 @@ SystemType::GetInstance().get());               \
 static SystemType##AutoRegister s_##SystemType##AutoRegisterInstance; \
 }
 
-#define REGISTER_COMPONENT_SYSTEM(ComponentType)                          \
-namespace {                                                               \
-    struct ComponentSystemAutoRegister_##ComponentType {                 \
-        ComponentSystemAutoRegister_##ComponentType() {                  \
-            SystemRegistry::Instance().Register(                         \
-                Components<ComponentType>());          \
-        }                                                                \
-    };                                                                   \
-    static ComponentSystemAutoRegister_##ComponentType                   \
-        instance_ComponentSystemAutoRegister_##ComponentType;            \
-}
 
 
 #endif //SYSTEMREGISTRY_H
